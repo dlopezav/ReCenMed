d5eb8e6de7104cbc166d7baa6b4998b520b0e917
Leonardo Delgado <sdelgadom@unal.edu.co>
2020-07-26 12:47:49 -0500

    Intentando navigation

---
+++ src/Components/Navigation.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class Navigation extends Component{
+    render(){
+        return(
+          <div className= "navbar navbar-light bg-light" id="nav">
+            <a className= "navbar-brand mr-auto"><strong>ReCenMed </strong></a>
+            <button className="btn btn-secondary">
+              Login
+            </button>
+          </div>
+        );        
+    }
+}
+
+export default Navigation;
