9361fa29130cb12274f9e940f41caac22aab935b
dlopezav <dlopezav@unal.edu.co>
2020-07-31 15:13:52 -0500

    localhost

--- src/App.js
+++ src/App.js
@@ -169,7 +169,7 @@ class App extends Component {
 
   componentWillMount() {
     request
-      .get('localhost:3000/getHospitals')
+      .get('/getHospitals')
       .end((err, res) => {
         console.log(JSON.parse(res.text));
         const hospitals = JSON.parse(res.text);
